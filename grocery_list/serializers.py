f7ffea65e6223dc7547e919d06b57419b35104b8
ilaria-barletta <114695123+ilaria-barletta@users.noreply.github.com>
2023-09-17 13:24:59 +0100

    create grocery list items when creating a list

--- grocery_list/serializers.py
+++ grocery_list/serializers.py
@@ -19,7 +19,19 @@ class GroceryListSerializer(serializers.ModelSerializer):
     creator = serializers.ReadOnlyField(source="creator.username")
     # DJ Rest documentation
     # https://www.django-rest-framework.org/api-guide/relations/#example
-    items = GroceryItemSerializer(many=True, read_only=True)
+    items = GroceryItemSerializer(many=True)
+
+    def create(self, validated_data):
+        items_data = validated_data.pop("items")
+        list = GroceryList.objects.create(**validated_data)
+
+        # Found here:
+        # https://stackoverflow.com/questions/30203652/how-to-get-request-user-in-django-rest-framework-serializer
+        user = self.context["request"].user
+
+        for item_data in items_data:
+            GroceryItem.objects.create(list=list, creator=user, **item_data)
+        return list
 
     class Meta:
         model = GroceryList
