f66a3210c4449dc79fc3e4645b23292b4a4cba63
ilaria-barletta <114695123+ilaria-barletta@users.noreply.github.com>
2023-10-08 12:27:50 +0200

    add created date field to grocery list

--- grocery_list/models.py
+++ grocery_list/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class GroceryList(models.Model):
     name = models.CharField(max_length=255)
+    create_date = models.DateField(auto_now_add=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     household = models.ForeignKey(Household, on_delete=models.CASCADE)
     is_complete = models.BooleanField()
