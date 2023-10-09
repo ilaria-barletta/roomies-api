383e85c193412958370f420bdb8aa628aec3767c
ilaria-barletta <114695123+ilaria-barletta@users.noreply.github.com>
2023-10-09 16:50:53 +0200

    add household name to grocery list serializer

--- grocery_list/serializers.py
+++ grocery_list/serializers.py
@@ -21,7 +21,16 @@ class GroceryItemSerializer(serializers.ModelSerializer):
 
 class GroceryListSerializer(serializers.ModelSerializer):
     creator = serializers.ReadOnlyField(source="creator.username")
+    household_name = serializers.ReadOnlyField(source="household.name")
 
     class Meta:
         model = GroceryList
-        fields = ["id", "creator", "household", "name", "is_complete", "create_date"]
+        fields = [
+            "id",
+            "creator",
+            "household",
+            "name",
+            "is_complete",
+            "create_date",
+            "household_name",
+        ]
